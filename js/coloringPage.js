6bf185d0af6a776afa048d8493773d1bcf4abd33
mskovv <maslay99@gmail.com>
2021-09-24 20:46:39 +0500

    more edit

--- js/coloringPage.js
+++ js/coloringPage.js
@@ -1,12 +1,8 @@
 $(document).ready(function () {
-	// let bol = $(".links__list_li ul li").addClass("activ");
-	// console.log(bol);
-
 	var url = window.location.href;
 
 	// passes on every "a" tag
 	$(".links__list_li ul li a").each(function () {
-		// checks if its the same on the address bar
 		if (url == (this.href)) {
 			$(this).parent().addClass("activ");
 		}
