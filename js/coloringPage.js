9cf738436578075c11a438cddb452d2bb1daf83e
mskovv <maslay99@gmail.com>
2021-09-24 18:41:41 +0500

    add cursor

--- js/coloringPage.js
+++ js/coloringPage.js
@@ -1,4 +1,14 @@
-$( document).ready(function() {
-	let bol = $("body").hasClass("header");
-	console.log(bol);
+$(document).ready(function () {
+	// let bol = $(".links__list_li ul li").addClass("activ");
+	// console.log(bol);
+
+	var url = window.location.href;
+
+	// passes on every "a" tag
+	$(".links__list_li ul li a").each(function () {
+		// checks if its the same on the address bar
+		if (url == (this.href)) {
+			$(this).parent().addClass("activ");
+		}
+	});
 });
