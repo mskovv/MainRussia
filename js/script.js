78a1d3b40271e9dab0a053f64d052b12e3870d4a
mskovv <maslay99@gmail.com>
2021-09-25 21:03:21 +0500

    second pages

--- js/script.js
+++ js/script.js
@@ -7,4 +7,6 @@ $(document).ready(function () {
 			$(this).parent().addClass("activ");
 		}
 	});
+
 });
+
---
+++ js/slider.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	$(".slider").slick({
+		slidesToShow:4,
+		slidesToScroll:4,
+		appendArrows:$(".others-bloggers__slider__frame_item")
+	});
+
+	// $(".")
+	// slidesToShow:4,
+	// slidesToScroll:4
+});
