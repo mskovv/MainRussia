49ae61e70e8ef78ed8246517e095f35f7b4d910b
mskovv <maslay99@gmail.com>
2021-09-27 01:04:21 +0500

    burger menu

--- js/script.js
+++ js/script.js
@@ -1,12 +1,16 @@
 $(document).ready(function () {
 	var url = window.location.href;
 
-	// passes on every "a" tag
+	
 	$(".links__list_li ul li a").each(function () {
 		if (url == (this.href)) {
 			$(this).parent().addClass("activ");
 		}
 	});
 
-});
+	$('.burger__header_container').click(function() {
+		$('.burger__header_container').toggleClass("change");
+		$('.burger__menu__list').toggleClass("brgr_activ");
+	});
 
+});
